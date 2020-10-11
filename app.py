a32531a5702b639cc376d823c58a361ca12e6c6f
Jade L. Lassery <48845323+jlassery@users.noreply.github.com>
2020-10-11 04:20:28 -0300

    Update app.py
    método POST

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def inicio():
 @app.route('/', methods=['POST'])
 def gravar():
     if database_enabled:
-        form = json.dumps(request.form, default=json_util.default)
+        form = request.json(request.form)
         db.forms.insert_one(form)
     return redirect('/')
 
